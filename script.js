abca56d688122238414573051d7c177bdf900459
Andos-trying-his-best <andoplotting66@gmail.com>
2025-12-24 23:40:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("loadboobies");
+const img = document.getElementById("boobies");
+
+button.addEventListener("click", () => {
+    img.src = "cabby-cabby-cat.gif";
+    button.textContent = "🫵😹"
+    });
